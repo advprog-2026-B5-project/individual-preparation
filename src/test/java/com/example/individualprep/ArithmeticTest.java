35b9ce8c24144fe19653451d29289547a8906848
roseliaevanny <roseliasucipto8@gmail.com>
2026-02-13 20:25:14 +0700

    add unit test for arithmetic subtract

--- src/test/java/com/example/individualprep/ArithmeticTest.java
+++ src/test/java/com/example/individualprep/ArithmeticTest.java
@@ -15,4 +15,15 @@ class ArithmeticTest {
         assertEquals(25.0, result);
     }
 
+    @Test
+    void testSubtract() {
+        double result1 = arithmeticUtility.subtract(5, 2);
+        assertEquals(3, result1);
+
+        double result2 = arithmeticUtility.subtract(0, 2);
+        assertEquals(-2, result2);
+
+        double result3 = arithmeticUtility.subtract(2, 2);
+        assertEquals(0, result3);
+    }
 }
