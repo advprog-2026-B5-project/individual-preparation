1d3bcb97a47eff0caf5d4cee3fd0d4986e87ed43
roseliaevanny <roseliasucipto8@gmail.com>
2026-02-13 21:01:21 +0700

    fix unit test

--- src/test/java/com/example/individualprep/VectorTest.java
+++ src/test/java/com/example/individualprep/VectorTest.java
@@ -25,12 +25,17 @@ class VectorTest {
         double[] v2 = {2.0, 1.0};
         double[] expected = {3.0, 4.0}; // (5-2, 5-1)
 
-        assertArrayEquals(expected, vectorUtility.subtract(v1, v2), "Vector subtraction result is incorrect");
+        assertArrayEquals(expected, vectorUtility.subtract(v1, v2), "Vector subtraction result is incorrect");}
+
+    @Test
     void testAddDifferentLength() {
         double[] v1 = {1.0, 2.0};
         double[] v2 = {3.0};
 
         assertThrows(IllegalArgumentException.class, () -> vectorUtility.add(v1, v2));
+    }
+
+    @Test
     void testDotProductValidInput() {
         double[] v1 = {1.0, 3.0, -5.0};
         double[] v2 = {4.0, -2.0, -1.0};
