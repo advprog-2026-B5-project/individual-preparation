bbac298b6cb4e15da03034e0aade384449273ecd
Inaeah <nayahsaffanah@gmail.com>
2026-02-13 20:18:19 +0700

    add vector-add unit test

---
+++ src/test/java/com/example/individualprep/VectorTest.java
@@ -0,0 +1,23 @@
+package com.example.individualprep;
+
+import com.example.individualprep.service.VectorUtility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    private final VectorUtility vectorUtility = new VectorUtility();
+
+    @Test
+    void testAdd() {
+        double[] v1 = {1.0, 2.0, 3.0};
+        double[] v2 = {4.0, 5.0, 6.0};
+
+        double[] result = vectorUtility.add(v1, v2);
+
+        assertArrayEquals(new double[]{5.0, 7.0, 9.0}, result);
+    }
+
+
+}
